dc76078ce10a9b03af71983a9c0f84168eb5c626
HybridDeath <hyperek635@gmail.com>
2026-07-28 01:06:39 +0200

    Add ContentParserService.cs, add instantation, use switch expr.

---
+++ GenericParserApi/Services/ContentParserService.cs
@@ -0,0 +1,33 @@
+﻿using GenericParserApi.Models;
+using GenericParserApi.Parsers;
+
+namespace GenericParserApi.Services
+{
+    public class ContentParserService
+    {
+        private readonly CsvParser _csvParser;
+        private readonly InternalJsonParser _jsonParser;
+
+        public ContentParserService()
+        {
+            _csvParser = new CsvParser();
+            _jsonParser = new InternalJsonParser();
+        }
+
+
+        public object Parse(ContentType type, string content)
+        {
+            return type switch
+            {
+                ContentType.CSV => 
+                    _csvParser.Parse(content),
+
+                ContentType.INTERNAL_JSON =>
+                    _jsonParser.Parse(content),
+
+                _ =>
+                    throw new NotSupportedException()
+            };
+        }
+    }
+}
