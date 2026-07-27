1a31a8d4a5436612aa5bc3faa4494fa55b4b45bd
HybridDeath <hyperek635@gmail.com>
2026-07-28 01:05:18 +0200

    Add request validator, remove request null check.

---
+++ GenericParserApi/Validators/RequestValidator.cs
@@ -0,0 +1,25 @@
+﻿using GenericParserApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericParserApi.Validators
+{
+    public static class RequestValidator
+    {
+        public static string? Validate(ParseRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Content))
+            {
+                return "Content was null/empty.";
+            }
+
+            if (!Enum.IsDefined(request.Type))
+            {
+                return "Unsupported request type.";
+            }
+
+            return null;
+        }
+    }
+}
