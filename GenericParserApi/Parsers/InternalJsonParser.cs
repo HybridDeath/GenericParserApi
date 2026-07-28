19870978171d5457ae6eee597f14bce99b60f974
HybridDeath <hyperek635@gmail.com>
2026-07-28 02:14:14 +0200

    Change object to ParserResult to accommodate for record count

--- GenericParserApi/Parsers/InternalJsonParser.cs
+++ GenericParserApi/Parsers/InternalJsonParser.cs
@@ -1,14 +1,20 @@
-﻿using System.Text.Json;
+﻿using GenericParserApi.Models;
+using System.Text.Json;
 
 namespace GenericParserApi.Parsers
 {
     public class InternalJsonParser : IContentParser
     {
-        public object Parse(string content)
+        public ParserResult Parse(string content)
         {
+            // TODO
             using var document = JsonDocument.Parse(content);
 
-            return document.RootElement;
+            return new ParserResult
+            {
+                Count = int.MaxValue,
+                Data = document.RootElement
+            };
         }
     }
 }
