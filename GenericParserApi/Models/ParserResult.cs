60cb032dc28c15af5b731b1aa3ae5c79945ec497
HybridDeath <hyperek635@gmail.com>
2026-07-28 02:14:35 +0200

    Add ParserResult

---
+++ GenericParserApi/Models/ParserResult.cs
@@ -0,0 +1,8 @@
+﻿namespace GenericParserApi.Models
+{
+    public record ParserResult
+    {
+        public int Count { get; init; }
+        public object Data { get; init; } = default!;
+    }
+}
