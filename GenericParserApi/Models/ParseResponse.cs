f7dc7ac35a0ca27d6d27aa4343ff38fff875da1f
HybridDeath <hyperek635@gmail.com>
2026-07-28 21:34:44 +0200

    Fix omit error null return for successful response

--- GenericParserApi/Models/ParseResponse.cs
+++ GenericParserApi/Models/ParseResponse.cs
@@ -3,11 +3,8 @@
     public record ParseResponse
     {
         public ParseStatus Status { get; init; }
-
         public int ProcessedCount { get; init; }
-
         public object? Data { get; init; }
-
         public string? Error { get; init; }
     }
 }
