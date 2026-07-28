da0cc65462c9c6931075b18c66a2f828bc2a8118
HybridDeath <hyperek635@gmail.com>
2026-07-28 02:15:55 +0200

    Add optional raw record field handling

--- GenericParserApi/Parsers/CsvParseException.cs
+++ GenericParserApi/Parsers/CsvParseException.cs
@@ -3,12 +3,18 @@
     [Serializable]
     internal class CsvParseException : Exception
     {
+        public string? RawRecord { get; }
+
         public CsvParseException()
         {
         }
 
-        public CsvParseException(string? message) : base(message)
+        public CsvParseException(
+            string message,
+            string? rawRecord = null)
+            : base(message)
         {
+            RawRecord = rawRecord;
         }
 
         public CsvParseException(string? message, Exception? innerException) : base(message, innerException)
