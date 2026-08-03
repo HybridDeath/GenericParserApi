830c5fd91bedfbaf1641b73525542cb098a2b77b
HybridDeath <hyperek635@gmail.com>
2026-08-03 03:53:40 +0200

    Add summary

--- GenericParserApi/Models/ParseRequest.cs
+++ GenericParserApi/Models/ParseRequest.cs
@@ -1,8 +1,19 @@
 ﻿namespace GenericParserApi.Models
 {
+    /// <summary>
+    /// Internal request DTO used to pass its fields to the parser.
+    /// </summary>
     public record ParseRequest
     {
+        /// <summary>
+        /// Gets the content type of the data to be parsed.
+        /// </summary>
         public ContentType Type { get; init; }
+
+        /// <summary>
+        /// Gets the content to be parsed.
+        /// </summary>
+        // Utrudniamy przekazanie null za pomocą string.Empty.
         public string Content { get; init; } = string.Empty;
     }
 }
